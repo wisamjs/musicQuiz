426267d660f39b602e44ba351dface07a11cc94f
wzaghal <wisamzaghal@gmail.com>
2014-05-25 23:22:24 -0400

    more changes to js,css

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,21 +1,21 @@
-//debugging
-var test;
-var mytest;
 
 $(document).ready(function(){
 
-
+	//Decided to practice using closures
 	var music = function(){
 		var apiKey = '697800e6a9fe10f5c42eab30c9ef6cb4';
 		var format = 'json';
-		//var ajaxParams = {'api_key': apiKey, 'format': 'json'};
 		var artists=[];
 		var response;
 		var score = 0;
 		var lives = 3;
 
 		return{
-			//calls ajax request
+			//Calls ajax request that
+			//accepts additional ajax parameters
+			//an index used for a callback
+			//and a callback function referred
+			//to as parse
 			getResponse: function(ajaxParams,callIndex,parse){
 				ajaxParams.api_key = apiKey;
 				ajaxParams.format = format;
@@ -29,33 +29,47 @@ $(document).ready(function(){
 					}
 				});
 			},
-			//adds similar artists to the ones provided
-			//to the artists array.
+			//Adds similar artists to the ones
+			//in the artist array.
 			getSimilarArtists:function(){
 				for (var i = 0; i< artists.length; i++){
 
 					//quick hack to get music.generate to work after the last ajax call.
+					//had issues with asynchronouse calls, so used an if statement
+					//to call additional parameters once all previous
+					//api calls in the loop are made.
+					// I encounter a similar situation when I call generate()
+					// but I fix this problem by using the 'callIndex' parameter
 					if (i === artists.length -1 ){
 						this.getResponse({'artist':artists[i],'method':'artist.getsimilar'},i,function(response){
 							
-						
+							//Get random number between 1 and the number of albums
+							//the artist has (not inclusive)
 							var numArtists = response.similarartists.artist.length;
 							var randomNum = _.random(1,numArtists-1);
 							var artist = response.similarartists.artist[randomNum].name;
 							
+							//Loop to make sure we only add unique artists
+							//that haven't been added before
 							while (artists.indexOf(artist) === -1){
 								if (artists.indexOf(artist) !== -1){
+									//if artist already exists, get another random artist.
+									//Will make a note to improve this in case next artist
+									//is also not unique
 									randomNum = randomNum = _.random(1,numArtists-1);
 									artist = response.similarartists.artist[randomNum].name;
 									
 								}
-								music.addArtist(artist);
-								//music.shuffleArtists();
 
-								music.generate(function(){
-									music.display();
-								});
+								music.addArtist(artist);
+
+
 							}
+
+							//generate data and display on callback
+							music.generate(function(){
+								music.display();
+							});
 						
 						});
 					}
@@ -63,13 +77,19 @@ $(document).ready(function(){
 						this.getResponse({'artist':artists[i],'method':'artist.getsimilar'},i,function(response){
 							
 						
-							
+							//Get random number between 1 and the number of albums
+							//the artist has (not inclusive)							
 							var numArtists = response.similarartists.artist.length;
 							var randomNum = _.random(1,numArtists-1);
 							var artist = response.similarartists.artist[randomNum].name;
-							
+
+							//Loop to make sure we only add unique artists
+							//that haven't been added before							
 							while (artists.indexOf(artist) === -1){
 								if (artists.indexOf(artist) !== -1){
+									//if artist already exists, get another random artist.
+									//Will make a note to improve this in case next artist
+									//is also not unique									
 									randomNum = randomNum = _.random(1,numArtists-1);
 									artist = response.similarartists.artist[randomNum].name;
 					
@@ -80,15 +100,8 @@ $(document).ready(function(){
 						
 						});
 					}
-				}
-
-			
-
-				
-				
-				
+				}	
 			},
-
 			//adds artist to array
 			addArtist: function(artist){
 				artists.push(artist);
@@ -97,15 +110,18 @@ $(document).ready(function(){
 			getArtists: function(){
 				return artists;
 			},
+			//shuffles order of artist array
 			shuffleArtists: function(){
 				artists = _.shuffle(artists);
+			},
+			//removes all artists from array
+			clearArtists: function(){
+				artists = [];
+
 			},
 			//generates data for trivia
 			//by making multiple ajax calls
 			generate: function( display){
-				//pick a randomNum answer.
-				//parse api req for answer.
-
 				
 				var randNum;
 				var albumArtistMap = [];
@@ -117,7 +133,7 @@ $(document).ready(function(){
 					
 					randNum = _.random(1,this.getArtists().length-1);
 
-					//if unique random number, add to array
+					//if unique random number, add to array and artist-album map
 					if (randArtists.indexOf(this.getArtists()[randNum]) === -1){
 						randArtists.push(this.getArtists()[randNum]);
 						albumArtistMap.push({'artist':this.getArtists()[randNum]});
@@ -128,8 +144,9 @@ $(document).ready(function(){
 
 
 
-				//ajax call for each random artist to get album
+				//ajax call for each random artist in map to get album
 				for (var i = 0; i< albumArtistMap.length; i++){
+
 					var artist = albumArtistMap[i].artist;
 					//ajax call for each random number to get album name
 					this.getResponse({'artist':artist,'method':'artist.gettopalbums'},i, function(response, callIndex){
@@ -146,15 +163,13 @@ $(document).ready(function(){
 						}
 						if (callIndex === albumArtistMap.length -1 ){
 							
-							//set current question
+							//save question, answer and albumArtistMap
 							trivia.setCurrentQuestion(albumArtistMap[callIndex].artist);
-
-							//set current answer
 							trivia.setCurrentAnswer(albumArtistMap[callIndex].album);
-
 							trivia.addTriviaMap(albumArtistMap);
 						}
 
+						//call display callback
 						display();
 					
 					});						
@@ -162,8 +177,10 @@ $(document).ready(function(){
 					
 				}
 			},
+			//Displays data onto the html
 			display: function(){
 				trivia.shuffleTriviaMap();
+
 				//remove classes from previous question
 				$('li').removeClass('correct');
 				$('li').removeClass('incorrect');
@@ -178,20 +195,41 @@ $(document).ready(function(){
 				}
 
 			},
+			//adds 5 points to score
 			updateScore: function(){
 				score= score + 5;
 			},
+			//returns the score
 			getScore: function(){
 				return score;
 			},
-			loseLife: function(){
-				lives = lives -1;
+			//sets the score to 0
+			resetScore: function(){
+				score = 0;
 			},
-			init: function(){
-				music.addArtist('Led Zeppelin');
-				music.addArtist('Pink Floyd');
-				music.addArtist('Eagles');
-				music.addArtist('Wings');
+			//reduces user 'lives' by 1
+			//on the screen and in variable
+			loseLife: function(){
+				$('.lives-icon'+lives).fadeOut();
+				lives--;
+			},
+			//returns the number of lives
+			getLives: function(){
+				return lives;
+			},
+			//resets lives to 0,
+			//and resets any hidden lives on screen
+			resetLives: function(){
+				$( ".life" ).each(function( index ) {
+					$(this).fadeIn();
+				});
+				lives = 3;
+			},
+			//init function that begins the process
+			init: function(artist1,artist2,artist3){
+				music.addArtist(artist1);
+				music.addArtist(artist2);
+				music.addArtist(artist3);
 
 				music.getSimilarArtists();
 
@@ -200,33 +238,58 @@ $(document).ready(function(){
 
 	}();
 
-	test = music;
-	music.init();
+	//displays initial user view
+	$('.quiz').hide();
+	$('.results').hide();
+
+	//user submits form
+	$( "form" ).on( "submit", function( event ) {
+  		event.preventDefault();
+  		music.init($('.artist1').val(),$('.artist2').val(),$('.artist3').val());
+  		$('.user-input').fadeOut();
+  		$('.quiz').fadeIn();
+
+	});
 
 	//clicking on choice
-	$('li').on('click',function(){
+	$('.quiz li').on('click',function(){
 		// check answer
 		var isCorrect = trivia.verifyChoice($(this).text());
 
 		if (isCorrect){
 			$(this).addClass('correct');
 			music.updateScore();
-			console.log('SCORE IS :   ' + music.getScore());
 
 
 			
 		}else{
 			$(this).addClass('incorrect');
 			music.loseLife();
-		}
+			if (music.getLives() === 0){
+				$('.quiz').hide();
+				$('.results').fadeIn();
+				$('.results').append('<p>Final Score: ' + music.getScore()+ ' </p>');
 
+
+
+			}
+		}
+		//get next question
+		//by regenerating
 		music.generate(function(){
 			music.display();
 		});
-		// if correct, change colour to green
 
-		//if incorrect, change colour to red
-		//and show correct answer in green
+	});
+
+	//user hits 'Start Over'
+	$('.restart').on('click',function(){
+		music.resetScore();
+		music.resetLives();
+		music.clearArtists();
+		$('.quiz').fadeOut();
+		$('.results').fadeOut();
+		$('.user-input').fadeIn();
 
 
 
--- app/scripts/trivia.js
+++ app/scripts/trivia.js
@@ -2,59 +2,52 @@
 
 var trivia = function() {
 	var allQuestions = ['{artist} produced the album:','Which artist created the album {album}?'];
-  	var currentTrivia={'question':'{artist} produced the album:'};
+  	var currentTrivia={'question':'produced the album:'};
   	var triviaChoices=[];
   	var triviaMap = [];
 
   return {
-  	
-  	getCurrentQuestion: function() {
-  		return currentTrivia.question;
-  	},
-	//returns placeholder for currentQuestion
-	getPlaceholder: function(){
-		var currQuestion = this.getCurrentQuestion();
-		return currQuestion.substring(currQuestion.indexOf('{'),currQuestion.indexOf('}')+1);
-	},
-	getCurrentAnswer: function(){
-		return currentTrivia.answer;
-	},
-	setCurrentAnswer: function(answer){
-		currentTrivia.answer=answer;
-	},
-	//sets currentQuestion by using the replace function
-	//to replace the handlebars with content
-	setCurrentQuestion: function(artist) {
-		this.replace(artist,'{artist} ');
-	},
-	//resets the currentQuestion to 
-	//its original placeholder form
-	reset: function(){
-		currentTrivia.question = allQuestions[0];
-	},
-	//replaces placeholder with the value
-	replace: function(value,placeholder){
-		currentTrivia.question = currentTrivia.question.substring(0,currentTrivia.question.indexOf(placeholder))+
-		value+' '+currentTrivia.question.substring(currentTrivia.question.indexOf(placeholder)+placeholder.length,currentTrivia.question.length);
-	},
-	addTriviaMap: function(obj){
-		triviaMap = obj;
-	},
-	getTriviaMap: function(obj){
-		return triviaMap;
-	},
-	shuffleTriviaMap: function(){
-		triviaMap = _.shuffle(triviaMap);
-	},
-	//check if album user selected
-	//belongs
-	verifyChoice: function(choice){
-		if (trivia.getCurrentAnswer() === undefined){
-			console.log('ERROR UNDEFINED');
-		}
-		var userSubmitted = trivia.getCurrentAnswer() === choice;
-		return userSubmitted;
+  		getCurrentQuestion: function() {
+  			return currentTrivia.question;
+  		},
+		//returns placeholder for currentQuestion
+		getPlaceholder: function(){
+			var currQuestion = this.getCurrentQuestion();
+			return currQuestion.substring(currQuestion.indexOf('{'),currQuestion.indexOf('}')+1);
+		},
+		getCurrentAnswer: function(){
+			return currentTrivia.answer;
+		},
+		setCurrentAnswer: function(answer){
+			currentTrivia.answer=answer;
+		},
+		//sets currentQuestion
+		setCurrentQuestion: function(artist) {
+			currentTrivia.question = artist + ' '+ 'produced the album:';
+		},
+		//resets the currentQuestion to 
+		//its original placeholder form
+		reset: function(){
+			currentTrivia.question = allQuestions[0];
+		},
+		//replaces placeholder with the value
+		addTriviaMap: function(obj){
+			triviaMap = obj;
+		},
+		getTriviaMap: function(obj){
+			return triviaMap;
+		},
+		shuffleTriviaMap: function(){
+			triviaMap = _.shuffle(triviaMap);
+		},
+		//check if choice is correct
+		verifyChoice: function(choice){
+			if (trivia.getCurrentAnswer() === undefined){
+				console.log('ERROR UNDEFINED');
+			}
+			var userSubmitted = trivia.getCurrentAnswer() === choice;
+			return userSubmitted;
 
-	}
+		}
   };
 }();
